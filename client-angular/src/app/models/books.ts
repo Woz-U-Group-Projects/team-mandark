0c24d3a4d68b34676fc1b06c5bb9bfdbd732c662
Braxton <onereanimatorwest@gmail.com>
2020-04-07 17:12:48 -0500

    models are up

---
+++ client-angular/src/app/models/books.ts
@@ -0,0 +1,10 @@
+export class Books {
+    book_id: number;
+    title: string;
+    author: string;
+    publisher: string;
+    description: string;
+    pub_date: number;
+    chk_out_date: number;
+    chkd_out: boolean;
+}
