3ee547646921fdd77b5cbc021b0c6ccc4d21e895
Braxton <onereanimatorwest@gmail.com>
2020-04-16 19:51:03 -0500

    Cleaned the old imports out

--- client-angular/src/app/app-routing.module.ts
+++ client-angular/src/app/app-routing.module.ts
@@ -1,24 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LogInComponent } from './log-in/log-in.component';
-import { HomeComponent } from './modules/home/home.component';
-import { LibraryComponent } from './library/library.component';
-import { UserAccountComponent } from './user-account/user-account.component';
-import { UserEditAccountComponent } from './user-edit-account/user-edit-account.component';
-import { DbUsersComponent } from './db-users/db-users.component';
-import { DbBooksComponent } from './db-books/db-books.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'log-in', component: LogInComponent },
-  { path: 'user-account', component: UserAccountComponent },
-  { path: 'library', component: LibraryComponent },
-  { path: 'user-edit-account', component: UserEditAccountComponent },
-  { path: 'db-users', component: DbUsersComponent },
-  { path: 'db-books', component: DbBooksComponent },
-];
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -3,32 +3,23 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
-
 import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { TaskDisplayComponent } from "./task-display/task-display.component";
-
-import { LogInComponent } from './log-in/log-in.component';
-import { HomeComponent } from './modules/home/home.component';
-import { LibraryComponent } from './library/library.component';
-import { UserAccountComponent } from './user-account/user-account.component';
-import { UserEditAccountComponent } from './user-edit-account/user-edit-account.component';
-import { DbUsersComponent } from './db-users/db-users.component';
-import { DbBooksComponent } from './db-books/db-books.component';
-import { AdminAccountComponent } from './admin-account/admin-account.component';
-import { NavbarComponent } from './Shared/components/navbar/navbar.component';
-
 import { AccountModule } from "./modules/account/account.module";
 
-
-
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from './Shared/components/navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [AppComponent, TaskDisplayComponent, LogInComponent, HomeComponent, LibraryComponent, UserAccountComponent, UserEditAccountComponent, DbUsersComponent, DbBooksComponent, AdminAccountComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, AccountModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
 
